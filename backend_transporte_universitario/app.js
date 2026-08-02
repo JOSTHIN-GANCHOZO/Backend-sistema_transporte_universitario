ebad5ff80ce0510c93a384efebe72aea043ad710
lamasbrava101-eng <lamasbrava101@gmail.com>
2026-08-01 19:13:20 -0500

    Routes de Permisos

--- backend_transporte_universitario/app.js
+++ backend_transporte_universitario/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import viajeRoutes from './router/ViajeRouter.js';
 import rolRoutes from './router/RolRouter.js';
+import permisoRoutes from './router/PermisoRouter.js';
 
 const app = express();
 app.use(cors());
@@ -11,4 +12,5 @@ app.use(express.json());
 // ===============================
 app.use('/api/viajes', viajeRoutes);
 app.use('/api/roles', rolRoutes);
+app.use('/api/permisos', permisoRoutes);
 export default app;
